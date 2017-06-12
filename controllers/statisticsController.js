db8d47259cc3dadc6d03866be768258a498dd99d
Paul Grubbs <jpgrubbs@crimson.ua.edu>
2017-06-12 16:59:00 -0500

    Add ability to view statistics for any given song
    Finish stubbing out view of statistics using d3js

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -4,37 +4,44 @@ if(process.env.NODE_ENV === 'production'){
   apiString = "http://avett-statistics.herokuapp.com/rest";
 }
 
-var amphitheatre, theatre, fest, arena, occurrences, numAmphitheatre, numTheatre, numFest, numArena;
-amphitheatre = theatre = fest = arena = occurrences = numAmphitheatre = numTheatre = numFest = numArena = 0;
+var totAmphitheatre, totTheatre, totFestival, totArena, occurrences, numAmphitheatre, numTheatre, numFestival, numArena;
+
 
 
 exports.get_statistics = function(req,res){
+  totAmphitheatre = totTheatre = totFestival = totArena = occurrences = numAmphitheatre = numTheatre = numFestival = numArena = 0;
   request.get(`${apiString}/artist`).query({id: "0a176d0a-ef46-4e7f-b018-9f4d65614668"}).end(
     function(err,result){
       var songName = req.query.song;
+      var fromYear = req.query.fromYear;
       var setlists = result.body.setlists;
       for(var i in setlists){
+        var date = setlists[i]['@eventDate'];
+        var year = date.split("-")[2];
+        if(parseInt(year,10)<parseInt(fromYear,10)) break;
         var venue = setlists[i].venue;
         var venueName = venue['@name'];
-        addVenueTally(venueName);
+        addVenueTally(venueName,year);
         var sets = setlists[i].sets.set;
         if(!sets){continue;}
         if(sets.constructor === Array){
           for(var j in sets){
             var songs = sets[j].song;
+            if(venueName.toLowerCase().includes("traverse")){
+            }
             if(songs.constructor === Array){
               for(var k in songs){
                 var song = songs[k];
                 var name = song['@name'];
                 if(name.toLowerCase() === songName.toLowerCase()){
-                  addSongTally(venueName);
+                  addSongTally(venueName,year);
                   occurrences++;
                 }
               }
             } else {
-              var name = song['@name'];
+              var name = songs['@name'];
               if(name.toLowerCase() === songName.toLowerCase()){
-                addSongTally(venueName);
+                addSongTally(venueName,year);
                 occurrences++;
               }
             }
@@ -46,46 +53,67 @@ exports.get_statistics = function(req,res){
               var song = songs[k];
               var name = song['@name'];
               if(name.toLowerCase() === songName.toLowerCase()){
-                addSongTally(venueName);
+                addSongTally(venueName,year);
                 occurrences++;
               }
             }
           } else {
-            var name = song['@name'];
+            var name = songs['@name'];
             if(name.toLowerCase() === songName.toLowerCase()){
-              addSongTally(venueName);
+              addSongTally(venueName,year);
               occurrences++;
             }
           }
         }
       }
-      console.log(occurrences);
-      res.send({amphitheatre:amphitheatre/numAmphitheatre, theatre:theatre/numTheatre, arena:arena/numArena, fest:fest/numFest,
-                numAmp:amphitheatre, numTheatre:theatre, numArena:arena, numFest:fest});
+      var returnObject = {
+        song: songName,
+        venueData: [
+        {
+          venue: "amphitheatre",
+          percentage: (numAmphitheatre/totAmphitheatre)*100
+        },
+        {
+          venue: "theatre",
+          percentage: (numTheatre/totTheatre)*100
+        },
+        {
+          venue: "arena",
+          percentage: (numArena/totArena)*100
+        },
+        {
+          venue: "festival",
+          percentage: (numFestival/totFestival)*100
+        }
+      ]};
+      res.send(returnObject);
+      console.log(totTheatre);
+      console.log(numTheatre);
+                //numAmp:numAmphitheatre, numTheatre:numAheatre, numArena:numArena, numFest:numFest});
     });
 
-    addSongTally = function(venue){
+    addVenueTally = function(venue, year){
       venue = venue.toLowerCase();
-      if(venue.includes("fest")||venue.includes("festival")){
-        fest++;
+      if(venue.includes("fest")||venue.includes("festival")||venue.includes(year)){
+        totFestival++;
       }
-      else if(venue.includes("arena")||venue.includes("coliseum")||venue.includes("civic center")){
-        arena++;
+      else if(venue.includes("arena")||venue.includes("coliseum")||venue.includes("civic center")||venue.includes("bowl")){
+        totArena++;
       }
       else if(venue.includes(" theatre")||venue.includes(" theater")||venue.includes("hall")){
-        theatre++;
+        totTheatre++;
       }
       else if(venue.includes("amphitheatre")||venue.includes("amphitheater")||venue.includes("park")||venue.includes("pavilion")){
-        amphitheatre++;
+        totAmphitheatre++;
       }
     }
 
-    addVenueTally = function(venue){
+    addSongTally = function(venue, year){
       venue = venue.toLowerCase();
-      if(venue.includes("fest")||venue.includes("festival")){
-        numFest++;
+      if(venue.includes("fest")||venue.includes("festival")||venue.includes(year)){
+        numFestival++;
       }
-      else if(venue.includes("arena")||venue.includes("coliseum")||venue.includes("civic center")){
+      else if(venue.includes("arena")||venue.includes("coliseum")||venue.includes("civic center")||venue.includes("bowl")){
         numArena++;
       }
       else if(venue.includes(" theatre")||venue.includes(" theater")||venue.includes("hall")){
